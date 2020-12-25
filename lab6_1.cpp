9efc07bae33fcef5f4066fe3d08aa895950c3f36
zen138yanatib <64641188+zen138yanatib@users.noreply.github.com>
2020-12-25 17:06:01 +0700

    Update lab6_1.cpp

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
+    double a  =  20, b = 30 ,c = 4;
+    double output;
 
-    outpot = a - b/c;
-    count << output: << output << \n;
+    output = a - b/c;
+    cout << "output: " << output << endl;
     return 0;
+}
+
 }
